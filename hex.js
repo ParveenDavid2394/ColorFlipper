bc3b10a3ddc14d6b8c384785adc91a0fb47682a8
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-11 11:00:47 +0800

    hex color generator

--- hex.js
+++ hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// get the button and span 
+const button = document.getElementById('btn');
+const span = document.querySelector('.color');
+
+button.addEventListener('click', ()=>{
+    // initialise the hexColor string
+    let hexColor = '#';
+
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNum(hex)];
+    }
+
+    document.body.style.backgroundColor = hexColor;
+    span.textContent = hexColor;
+})
+
+function getRandomNum(arr) {
+    return Math.floor(Math.random() * arr.length)
+}
